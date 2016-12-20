82d604c48abccd1ca8092d4dab1bcdb604a53f86
Philip Marian <philip.marian@mytfs.ca>
2016-12-19 21:51:20 -0500

    Version1.2
    Commit to Sync Project

--- 3dPong/Assets/Scripts/PowerUp.cs
+++ 3dPong/Assets/Scripts/PowerUp.cs
@@ -3,20 +3,37 @@ using System.Collections;
 
 public class PowerUp : MonoBehaviour {
     GameObject Ball;
+    GameObject PowerUpPrefab;
     float debufftimer;
     float bufftimer;
+   
+    public Transform SpawnPointOne;
+    public Transform SpawnPointTwo;
+    public Transform SpawnPointThree;
+    public Transform SpawnPointFour;
+    public Transform SpawnPointFive;
 
-    Transform SpawnPointOne;
-    Transform SpawnPointTwo;
-    Transform SpawnPointThree;
-    Transform SpawnPointFour;
-    Transform SpawnPointFive;
-    
+    int spawnPoint;
+    float lifeTime;
+    float respawnTime;
+
     void Start () {
         Ball = GameObject.Find("Ball");
-	}
+        PowerUpPrefab = GameObject.Find("PowerUp");
+        spawnPoint = Random.Range(0, 4);
+        respawnTime = Random.Range(10, 15);
+
+    }
 	
 	void Update () {
+        if (Input.GetKeyDown("c"))
+        {
+            Instantiate(PowerUpPrefab, SpawnPointOne.position, SpawnPointOne.rotation);
+            Instantiate(PowerUpPrefab, SpawnPointTwo.position, SpawnPointTwo.rotation);
+            Instantiate(PowerUpPrefab, SpawnPointThree.position, SpawnPointTwo.rotation);
+            Instantiate(PowerUpPrefab, SpawnPointFour.position, SpawnPointTwo.rotation);
+            Instantiate(PowerUpPrefab, SpawnPointFive.position, SpawnPointTwo.rotation);
+        }
         if (Input.GetKey("z"))
         {
             Debug.Log(debufftimer);
@@ -35,6 +52,11 @@ public class PowerUp : MonoBehaviour {
                 bufftimer = 5.0f;
             }
         }
+        if (respawnTime<0)
+        {
+
+        }
+
     }
 
     void onCollisionEnter(Collision c)
